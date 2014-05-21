86378c78aa3c043c72bb215fd74aeae79fd03225
Benoit Hediard <ben@agorapulse.com>
2014-05-21 10:06:32 +0200

    Coveralls code coverage integration added

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,11 +9,19 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
+        // Latest httpcore and httpmime
+        build 'org.apache.httpcomponents:httpcore:4.3.2'
+        build 'org.apache.httpcomponents:httpclient:4.3.2'
+        build 'org.apache.httpcomponents:httpmime:4.3.3'
+        // Latest Segment.io analytics java client
         compile 'com.github.segmentio:analytics:0.4.2'
+        // Latest Joda Time
         compile 'joda-time:joda-time:2.3'
     }
     plugins {
-        build(':release:3.0.1') {
+        build(':release:3.0.1',
+                ':rest-client-builder:1.0.3',
+                ':coveralls:0.1') {
             export = false
         }
     }
